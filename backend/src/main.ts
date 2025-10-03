9b9882a16ff13464e0192877bbd5a2e407c7824f
Antoine0703 <antoine.beal@epitech.eu>
2025-10-03 15:38:53 +0200

    feat(story1.1): add implementation of story1.1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Enable CORS for web client
+  app.enableCors({
+    origin: ['http://localhost:8081'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 8080);
 }
-bootstrap();
+void bootstrap();
